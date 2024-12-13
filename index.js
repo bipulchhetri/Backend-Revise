1ca229427042c01acd54d8458860ea26ff6a1892
bipulchhetri <bipulchhetri80@gmail.com>
2024-12-13 19:50:52 +0530

    Js basic update

---
+++ index.js
@@ -0,0 +1,41 @@
+let a=4;
+let b=2;
+let ans=a+b;
+console.log(ans)
+
+//array
+
+const car=['sav','bmw','tesla'];
+car.push("tata")
+console.log(car)
+
+var age=17;
+if(age<18)
+{
+    console.log("YOU CAN'T VOTE")
+}
+else{
+    console.log("you can vote")
+}
+
+let x=10;
+for(let i=0;i<=x;i++)
+{
+    console.log(i)
+}
+
+const person={
+    name:"Bipul",
+    age:19,
+    isStudent:false
+};
+console.log(person)
+
+const ages=[15,18,10,19];
+const result=ages.filter(Checkage);
+function Checkage(ages)
+{
+    return ages<18
+}
+console.log(result)
+
