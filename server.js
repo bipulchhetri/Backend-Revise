e392238d6839df628062f5e5a1cc4a63a0bfe8e8
bipulchhetri <bipulchhetri80@gmail.com>
2024-12-14 22:06:55 +0530

    Server updated

--- server.js
+++ server.js
@@ -12,20 +12,34 @@
 // var age=notes.age;
 // console.log(age)
 
+// const express=require('express')
+// const app=express();
+// app.get('/',(req,res)=>
+// {
+//     res.send("Express server sent this")
+// })
+
+// app.get('/op', (req,res)=>
+// {
+   
+//     let customize={
+//         name:"Random",
+//         size:"L"
+//     }
+//     res.send(customize)
+// })
+// app.listen(3000)
+
 const express=require('express')
 const app=express();
-app.get('/',(req,res)=>
-{
-    res.send("Express server sent this")
-})
 
-app.get('/op', (req,res)=>
-{
-   
-    let customize={
-        name:"Random",
-        size:"L"
-    }
-    res.send(customize)
+app.get('/',(req,res)=>{
+    res.send("I am home page ")
 })
-app.listen(3000)
+app.get('/hello',(req,res)=>
+{
+    res.send("Hello how are you")
+});
+app.listen(3000, () =>{
+    console.log("server is running ")
+})
